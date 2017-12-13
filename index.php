13bf32932303af3549695a49addb6ae87ce7cc5b
Pauline Michalowski <michalowski.pauline@hotmail.fr>
2017-12-13 15:25:53 +0100

    Création de l'exercice 2

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 2</title>
+</head>
+<body>
+  <?php
+  $little_string = 'Bonjour !';
+  function concatString($text) {
+    return $text;
+  }
+  echo concatString($little_string);
+  ?>
+</body>
+</html>
